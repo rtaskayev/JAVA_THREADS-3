c3d95fc3f3dffdacec9bbe8eb2d2566d74e5c66b
rtaskayev <washingtonqwerty@gmail.com>
2022-11-24 12:19:06 +0100

    JavaThreads. 4 - Call Center

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,113 +1,91 @@
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        AtomicInteger threeCharsCount = new AtomicInteger(0);
-        AtomicInteger fourCharsCount = new AtomicInteger(0);
-        AtomicInteger fiveCharsCount = new AtomicInteger(0);
+        BlockingQueue<Request> requests = new ArrayBlockingQueue<>(1000);
+        final int timeToProcessRequest = 100;
+        final int numberOfRequestsForSpecialistToProcess = 100;
+        final int timeForRequestsGenerationHold = 1000;
+        final int numberOfRequests = 60;
+        final int numberOfIterations = 5;
 
-        Random random = new Random();
-        String[] texts = new String[100_000];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abc", 3 + random.nextInt(3));
-            //System.out.println(texts[i]);
-        }
-
-        System.out.println("------------------------------------------");
-
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < texts.length; i++) {
-                    if (checkForSameChars(texts[i])) {
-                        //System.out.println("checkForSameChars - " + texts[i]);
-                        if (texts[i].length() == 3) threeCharsCount.getAndIncrement();
-                        if (texts[i].length() == 4) fourCharsCount.getAndIncrement();
-                        if (texts[i].length() == 5) fiveCharsCount.getAndIncrement();
+        // Thread to simulate first specialist
+        new Thread(() -> {
+            for (int i = 0; i < numberOfRequestsForSpecialistToProcess; i++) {
+                Request request = requests.poll();
+                if (request == null) {
+                    System.out.println("Queue is empty operator1");
+                }
+                else {
+                    try {
+                        Thread.sleep(timeToProcessRequest);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-            }
-
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < texts.length; i++) {
-                if (checkForPalindrome(texts[i])) {
-                    //System.out.println("checkForPalindrome - " + texts[i]);
-                    if (texts[i].length() == 3) threeCharsCount.getAndIncrement();
-                    if (texts[i].length() == 4) fourCharsCount.getAndIncrement();
-                    if (texts[i].length() == 5) fiveCharsCount.getAndIncrement();
+                    i--;
+                    System.out.println("Processed request operator1");
                 }
             }
+        }).start();
 
-        });
 
-        Thread thread3 = new Thread(() -> {
-            for (int i = 0; i < texts.length; i++) {
-                if (checkForCharsSequence(texts[i])) {
-                    //System.out.println("checkForCharsSequence - " + texts[i]);
-                    if (texts[i].length() == 3) threeCharsCount.getAndIncrement();
-                    if (texts[i].length() == 4) fourCharsCount.getAndIncrement();
-                    if (texts[i].length() == 5) fiveCharsCount.getAndIncrement();
+        // Thread to simulate second specialist
+        new Thread(() -> {
+            for (int i = 0; i < numberOfRequestsForSpecialistToProcess; i++) {
+                Request request = requests.poll();
+                if (request == null) {
+                    System.out.println("Queue is empty operator2");
+                }
+                else {
+                    try {
+                        Thread.sleep(timeToProcessRequest);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Processed request operator2");
+                    i--;
                 }
             }
+        }).start();
 
-        });
+
+        // Thread to simulate third specialist
+        new Thread(() -> {
+            for (int i = 0; i < numberOfRequestsForSpecialistToProcess; i++) {
+                Request request = requests.poll();
+                if (request == null) {
+                    System.out.println("Queue is empty operator3");
+                }
+                else {
+                    try {
+                        Thread.sleep(timeToProcessRequest);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Processed request operator3");
+                    i--;
+                }
+            }
+        }).start();
 
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-
-        thread1.join();
-        thread2.join();
-        thread3.join();
-
-        System.out.println("------------------------------------------");
-        System.out.println("threeCharsCount = " + threeCharsCount);
-        System.out.println("fourCharsCount = " + fourCharsCount);
-        System.out.println("fiveCharsCount = " + fiveCharsCount);
+        // Thread to simulate requests
+        new Thread(() -> {
+            for (int i = 0; i < numberOfIterations; i++) {
+                for (int j = 0; j < numberOfRequests; j++) {
+                    requests.add(new Request());
+                }
+                System.out.println("Generated 60 requests");
+                try {
+                    Thread.sleep(timeForRequestsGenerationHold);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
     }
-
-    public static String generateText(String letters, int length) {
-        Random random = new Random();
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return text.toString();
-    }
-
-    public static boolean checkForPalindrome(String string) {
-
-        String rev = "";
-
-        for (int i = string.length() - 1; i >= 0; i--) {
-            rev = rev + string.charAt(i);
-        }
-
-        return string.equals(rev);
-    }
-
-    public static boolean checkForCharsSequence(String string) {
-
-        char[] charset = string.toCharArray();
-
-        for (int i = 0; i < string.length() - 1; i++) {
-            if (charset[i] > charset[i + 1]) return false;
-        }
-        return true;
-    }
-
-    public static boolean checkForSameChars(String string) {
-
-        char[] charset = string.toCharArray();
-
-        for (int i = 1; i < charset.length; i++) {
-            if (charset[i] != charset[0]) return false;
-        }
-        return true;
-    }
-
 }
---
+++ src/main/java/Request.java
@@ -0,0 +1,6 @@
+public class Request {
+
+    private int id;
+
+    public Request() {}
+}
